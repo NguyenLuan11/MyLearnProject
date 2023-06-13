e06948a0b883fd403fd27a962cd928e8aa8f2e0a
Trần Nguyên Luân <nguyenluan0101020@gmail.com>
2023-06-13 15:51:14 +0700

    Add files via upload

---
+++ WhatsAppChat/index.js
@@ -0,0 +1,28 @@
+const sendText = document.querySelector('.sendMessage');
+const sendVideo_Audio = document.querySelector('.video_audio');
+const message = document.querySelector('.message');
+
+sendText.classList.add('hide');
+
+message.addEventListener('keyup', () => {
+    if(message.value != "") {
+        sendVideo_Audio.classList.add('hide');
+        sendText.classList.remove('hide');
+    } else {
+        sendVideo_Audio.classList.remove('hide');
+        sendText.classList.add('hide');
+    }
+})
+
+const content = document.querySelector('.content');
+const sendTextMessage = document.querySelector('.sendTextMessage');
+
+sendTextMessage.addEventListener('click', () => {
+    if(message.value != "") {
+        content.innerHTML += `
+            <p class="send">${message.value}</p>
+        `
+    }
+
+    message.value = "";
+})
